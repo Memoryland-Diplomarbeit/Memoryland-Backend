03581ef26d09e344974b2e62a590e03cc41364dc
HikariTempest <arwed@gmx.at>
2025-01-04 15:58:45 +0100

    feat: implemented the blob storage service that manages the photos

---
+++ memoryland-backend/BlobStoragePersistence/BlobStoragePhotoService.cs
@@ -0,0 +1,78 @@
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace BlobStoragePersistence;
+
+public class BlobStoragePhotoService
+{
+    private BlobServiceClient BlobSvcClient { get; set; }
+    private string AccessKey { get; }
+    
+    public BlobStoragePhotoService(BlobServiceClient blobServiceClient)
+    {
+        BlobSvcClient = blobServiceClient;
+        
+        // get the access key from the user secrets
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<BlobStoragePhotoService>()
+            .Build();
+        
+        var accessKey = config.GetValue<string>(
+            "ConnectionStrings:BlobStorageDefault");
+        
+        if (string.IsNullOrEmpty(accessKey))
+            throw new NullReferenceException(nameof(accessKey));
+        
+        foreach (var value in accessKey.Split(';'))
+        {
+            if (value.Contains("AccountKey"))
+            {
+                AccessKey = value.Replace("AccountKey=", "");
+                return;
+            }
+        }
+        
+        // null-reference exception since the access key
+        // isn't set in the config
+        throw new NullReferenceException(nameof(accessKey));
+    }
+
+    public async Task UploadPhoto(
+        string blobContainerName, 
+        string albumName, 
+        string photoName, 
+        byte[] photoBytes)
+    {
+        var containerClient = BlobSvcClient
+            .GetBlobContainerClient(blobContainerName);
+        await containerClient.CreateIfNotExistsAsync();
+
+        var blobClient = containerClient.GetBlobClient(
+            $"{albumName}/{photoName}");
+
+        using var stream = new MemoryStream(photoBytes);
+        await blobClient.UploadAsync(stream, overwrite: true);
+    }
+
+    public async Task<Uri?> GetPhoto(
+        string blobClientName, 
+        string albumName, 
+        string photoName)
+    {
+        var containerClient = BlobSvcClient
+            .GetBlobContainerClient(blobClientName);
+
+        if (!await containerClient.ExistsAsync()) return null;
+        
+        var blobClient = containerClient.GetBlobClient(
+            $"{albumName}/{photoName}");
+
+        if (!await blobClient.ExistsAsync()) return null;
+        
+        return SasTokenGeneratorService
+            .CreateUserDelegationSasBlob(
+                blobClient,
+                AccessKey);
+
+    }
+}
