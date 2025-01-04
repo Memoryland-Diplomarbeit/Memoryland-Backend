569c2a20bbd0dbe045c5665576d0fb81464649cb
HikariTempest <arwed@gmx.at>
2025-01-04 16:13:58 +0100

    feat: Initialised the authentication and the blob client in the program.cs file

--- memoryland-backend/WebApi/Program.cs
+++ memoryland-backend/WebApi/Program.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
 using BlobStoragePersistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Azure;
+using Microsoft.Identity.Web;
 using Persistence;
 using WebApi.Service;
 
@@ -8,6 +12,8 @@ builder.Configuration.AddUserSecrets<Program>();
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddSingleton<BlobStoragePhotoService>();
@@ -26,6 +32,39 @@ builder.Services.AddCors(options =>
         });
 });
 
+#region BlobStorage Initialisation
+
+var storageConnection = builder.Configuration["ConnectionStrings:BlobStorageDefault"];
+
+builder.Services.AddAzureClients(azureBuilder =>
+{
+    azureBuilder.AddBlobServiceClient(storageConnection);
+});
+
+#endregion
+
+#region Authentication and Authorization Initialisation
+
+// This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
+// By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
+// For instance, 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles' claim.
+// This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+// Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(
+        options => 
+        {
+            builder.Configuration.Bind("AzureAdB2C", options);
+        },
+        options =>
+        {
+            builder.Configuration.Bind("AzureAdB2C", options);
+        });
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,10 +74,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseCors("AllowAll");
 app.UseHsts();
 app.UseHttpsRedirection();
 
 app.MapGet("hello", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
