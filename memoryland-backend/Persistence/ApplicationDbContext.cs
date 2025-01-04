62925aa7a4304fcef3f7baca92589477014de9c4
HikariTempest <arwed@gmx.at>
2025-01-04 09:59:19 +0100

    feat: Implement persistence via the entityframework

---
+++ memoryland-backend/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly ILogger<ApplicationDbContext> _logger;
+
+    public ApplicationDbContext(ILogger<ApplicationDbContext> logger)
+    {
+        _logger = logger;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<PhotoAlbum> PhotoAlbums { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+    public DbSet<Memoryland> Memorylands { get; set; }
+    public DbSet<MemorylandType> MemorylandTypes { get; set; }
+    public DbSet<MemorylandConfiguration> MemorylandConfigurations { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) return;
+        
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<ApplicationDbContext>()
+            .Build();
+        
+        var useLocalDbIsValidValue = bool.TryParse(
+            configuration["UseLocalDb"], 
+            out var useLocalDb);
+
+        if (useLocalDbIsValidValue && useLocalDb)
+        {
+            _logger.Log(LogLevel.Information, "Using 'DefaultLocal' connection string");
+            optionsBuilder
+                .LogTo(
+                    msg => Debug.WriteLine(msg),
+                    LogLevel.Debug,
+                    DbContextLoggerOptions.SingleLine | DbContextLoggerOptions.UtcTime)
+                .UseNpgsql(configuration["ConnectionStrings:DefaultLocal"]);
+        }
+        else
+        {
+            _logger.Log(LogLevel.Information, "Using 'Default' connection string");
+            optionsBuilder
+                .LogTo(
+                    msg => Debug.WriteLine(msg),
+                    LogLevel.Debug,
+                    DbContextLoggerOptions.SingleLine | DbContextLoggerOptions.UtcTime)
+                .UseNpgsql(configuration["ConnectionStrings:Default"]); 
+        }
+    }
+}
