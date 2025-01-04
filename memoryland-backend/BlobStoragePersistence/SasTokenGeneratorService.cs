7089ad350af5775a49c421cfe4d86d4d1d956a4b
HikariTempest <arwed@gmx.at>
2025-01-04 15:57:47 +0100

    feat: created Project for blobstorage and blob sas token management and implemented a token generator

---
+++ memoryland-backend/BlobStoragePersistence/SasTokenGeneratorService.cs
@@ -0,0 +1,47 @@
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
+
+namespace BlobStoragePersistence;
+
+public static class SasTokenGeneratorService
+{
+    private static double TokenLifeTimeInHours => 2;
+
+    public static Uri CreateUserDelegationSasBlob(
+        BlobClient blobClient,
+        string accessKey)
+    {
+        var tokenStartTime = DateTimeOffset.Now;
+        var tokenEndTime = tokenStartTime
+            .AddHours(TokenLifeTimeInHours);
+        
+        var sasBuilder = new BlobSasBuilder()
+        {
+            BlobContainerName = blobClient.BlobContainerName,
+            BlobName = blobClient.Name,
+            Resource = "b", // Blob resource
+            StartsOn = tokenStartTime,
+            ExpiresOn = tokenEndTime
+        };
+
+        sasBuilder.SetPermissions(
+            BlobSasPermissions.Read | BlobSasPermissions.Write);
+
+        var blobSvcClient = blobClient
+            .GetParentBlobContainerClient()
+            .GetParentBlobServiceClient();
+        
+        var storageSharedKeyCredential = new StorageSharedKeyCredential(
+            blobSvcClient.AccountName,
+            accessKey);
+
+        var uriBuilder = new BlobUriBuilder(blobClient.Uri)
+        {
+            Sas = sasBuilder.ToSasQueryParameters(storageSharedKeyCredential)
+        };
+
+        return uriBuilder.ToUri();
+    }
+}
